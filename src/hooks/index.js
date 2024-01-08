abf9066c1c9cdfacbf54a41b196477e163352094
maikcodes <whunter0111@gmail.com>
2024-01-07 23:51:13 -0500

    add useModal

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,5 @@
 export { default as UseFetch } from './UseFetch'
+export { default as useModal } from './UseModal'
+export { default as UsePagination } from './UsePagination'
+export { default as UsePaginationIndices } from './UsePaginationIndices'
+export * from './index'
