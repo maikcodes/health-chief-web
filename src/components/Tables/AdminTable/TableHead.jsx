52834950f0683d5ee44fad465c5185877fff4aac
maikcodes <whunter0111@gmail.com>
2024-01-21 19:24:22 -0500

    add table header component

--- src/components/Tables/AdminTable/TableHead.jsx
+++ src/components/Tables/AdminTable/TableHead.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TableHead ({ children }) {
   return (
     <thead className='sticky top-0 h-[50px]'>
-      <tr className='uppercase bg-biscay-700 text-white text-left'>
+      <tr className='uppercase bg-biscay-700 text-white text-left font-bold'>
         {children}
       </tr>
     </thead>
---
+++ src/components/Tables/AdminTable/TableHeader.jsx
@@ -0,0 +1,7 @@
+function TableHeader ({ children }) {
+  return (
+    <th className='px-4 py-2 text-center'>{children}</th>
+  )
+}
+
+export default TableHeader
