a08495b97c1f3244a7f86d8951191de1753ab702
maikcodes <whunter0111@gmail.com>
2024-01-21 19:28:20 -0500

    Add FormInputText component.

---
+++ src/components/Forms/FormInputText.jsx
@@ -0,0 +1,20 @@
+import { TextInput } from '@components/Inputs'
+import { toCamelCase } from '@services/DataFormatters'
+
+function FormInputText ({ value, title, handleDataChange }) {
+  const elementName = toCamelCase(title)
+  return (
+    <div className='flex flex-col'>
+      <label className='font-bold' htmlFor={elementName}>{title}:</label>
+      <TextInput
+        name={elementName}
+        id={elementName}
+        value={value}
+        handleDataChange={handleDataChange}
+        placeholder={`Insert ${title}`}
+      />
+    </div>
+  )
+}
+
+export default FormInputText
