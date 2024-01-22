b17da8d23f2ba425a11fcf7cc342a5fcf5d14d23
maikcodes <whunter0111@gmail.com>
2024-01-21 19:25:41 -0500

    add DateInput component

---
+++ src/components/Inputs/DateInput.jsx
@@ -0,0 +1,14 @@
+function DateInput ({ name, id, value, handleDataChange }) {
+  return (
+    <input
+      className='px-2 py-1 border-2 border-gray-300 rounded-lg bg-gray-200 focus:outline-none focus:border-gray-200 focus:ring-2 focus:ring-biscay-700'
+      type='date'
+      name={name}
+      id={id}
+      value={value}
+      onChange={handleDataChange}
+    />
+  )
+}
+
+export default DateInput
--- src/components/Inputs/index.js
+++ src/components/Inputs/index.js
@@ -1,3 +1,3 @@
+export { default as DateInput } from './DateInput'
 export { default as SearchInput } from './SearchInput'
 export { default as TextInput } from './TextInput'
-export * from './index'
