b839c504e8831eda9dcc6c34e823ee1f72502e48
maikcodes <whunter0111@gmail.com>
2024-01-21 19:27:09 -0500

    Add FormInputDate component.

---
+++ src/components/Forms/FormInputDate.jsx
@@ -0,0 +1,19 @@
+import { DateInput } from '@components/Inputs'
+import { toCamelCase } from '@services/DataFormatters'
+
+function FormInputDate ({ value, title, handleDataChange }) {
+  const elementName = toCamelCase(title)
+  return (
+    <div className='flex flex-col'>
+      <label className='font-bold' htmlFor={elementName}>{title}:</label>
+      <DateInput
+        name={elementName}
+        id={elementName}
+        value={value}
+        handleDataChange={handleDataChange}
+      />
+    </div>
+  )
+}
+
+export default FormInputDate
