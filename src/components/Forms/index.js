3eceee242c4ad6156d22daa20c41ab4803b16d2e
maikcodes <whunter0111@gmail.com>
2024-01-21 19:29:10 -0500

    Add components with labels for forms.

---
+++ src/components/Forms/index.js
@@ -0,0 +1,3 @@
+export { default as FormInputDate } from './FormInputDate'
+export { default as FormInputSelect } from './FormInputSelect'
+export { default as FormInputText } from './FormInputText'
